09adcc29828df6797baa6baa66ccb716f0ee9ed1
jlippold <jlippold@datacation.com>
2015-02-21 15:49:16 -0500

    Initial Commit

---
+++ dtvRemote/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dtvRemote
+//
+//  Created by Jed Lippold on 2/21/15.
+//  Copyright (c) 2015 jed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
