6cf36bc4cfb9d415c7464afd2fe88cae109c8846
jlippold <jlippold@datacation.com>
2015-05-12 20:17:19 -0400

    adds basic vibrant buttons to scroll view

--- dtvRemote/CenterViewController.m
+++ dtvRemote/CenterViewController.m
@@ -675,6 +675,7 @@
 }
 
 - (void) showCommandSlider {
+    
     if (commandScrollView) {
         [commandScrollView removeFromSuperview];
         commandScrollView = nil;
@@ -684,48 +685,96 @@
         [commandPager removeFromSuperview];
         commandPager = nil;
     }
+
+    UIImageView *iv = [[UIImageView alloc] initWithImage:backgroundView.image];
+    iv.frame = [[UIScreen mainScreen] bounds];
+    iv.contentMode = UIViewContentModeScaleToFill;
+    [[[UIApplication sharedApplication] keyWindow] addSubview:iv];
+    
+    UIBlurEffect *blurEffect = [UIBlurEffect effectWithStyle:UIBlurEffectStyleDark];
+    UIVisualEffectView *effectView = [[UIVisualEffectView alloc] initWithEffect:blurEffect];
+    effectView.translatesAutoresizingMaskIntoConstraints = NO;
+    effectView.userInteractionEnabled = YES;
+    
+    UIVibrancyEffect *vibrance = [UIVibrancyEffect effectForBlurEffect:blurEffect];
+    UIVisualEffectView *newEffectView = [[UIVisualEffectView alloc] initWithEffect:vibrance];
+    newEffectView.translatesAutoresizingMaskIntoConstraints = NO;
+    newEffectView.userInteractionEnabled = YES;
+    
+    // Add the vibrance effect to our blur effect view
+    [effectView.contentView addSubview:newEffectView];
+    
+    [effectView.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[v]|" options:0 metrics:nil views: @{ @"v" : newEffectView }]];
+    [effectView.contentView addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|[v]|" options:0 metrics:nil views: @{ @"v" : newEffectView }]];
+    
+    CGRect frm = [[UIScreen mainScreen] bounds];
+    frm.size.height = 80;
+    frm.origin.x = 0;
+    frm.origin.y = 400;
+    
+    UILabel *test = [[UILabel alloc] initWithFrame:frm];
+    test.text = @"";
+    test.font = [UIFont fontWithName:@"Helvetica" size:30];
+    test.textAlignment = NSTextAlignmentCenter;
+    
+    // Add label to the vibrance content view.
+    [newEffectView.contentView addSubview:test];
+    [newEffectView.contentView addConstraint:[NSLayoutConstraint constraintWithItem:test attribute:NSLayoutAttributeCenterX relatedBy:NSLayoutRelationEqual toItem:newEffectView.contentView attribute:NSLayoutAttributeCenterX multiplier:1.0 constant:0.0]];
+    [newEffectView.contentView addConstraint:[NSLayoutConstraint constraintWithItem:test attribute:NSLayoutAttributeCenterY relatedBy:NSLayoutRelationEqual toItem:newEffectView.contentView attribute:NSLayoutAttributeCenterY multiplier:1.0 constant:0.0]];
+    
+    
     
     commandScrollView = [[UIScrollView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     commandScrollView.pagingEnabled = YES;
     commandScrollView.delegate = self;
     commandScrollView.showsHorizontalScrollIndicator = NO;
     
-    CGRect frm = [[UIScreen mainScreen] bounds];
+    frm = [[UIScreen mainScreen] bounds];
     frm.origin.y = frm.size.height - 30;
     frm.size.height = 10;
     commandPager = [[UIPageControl alloc] initWithFrame:frm];
     commandPager.numberOfPages = 3;
     commandPager.currentPage = 0;
     
+    [newEffectView.contentView addSubview:commandScrollView];
+    [newEffectView.contentView addSubview:commandPager];
+    
+    [[[UIApplication sharedApplication] keyWindow] addSubview:effectView];
+    
     [self setupScrollView:commandScrollView];
     
-    [[[UIApplication sharedApplication] keyWindow] addSubview:commandScrollView];
-    [[[UIApplication sharedApplication] keyWindow] addSubview:commandPager];
 
+    
 }
 
 - (void) setupScrollView:(UIScrollView*)scrollView {
     
-    UIImage *image1 = [Colors imageWithColor:[Colors greenColor]];
-    UIImage *image2 = [Colors imageWithColor:[Colors redColor]];
-    UIImage *image3 = [Colors imageWithColor:[Colors blueColor]];
-    NSArray *picArray = [[NSArray alloc] initWithObjects:image1, image2, image3, nil];
+    UIView *v1 = [[UIView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    UIView *v2 = [[UIView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    UIView *v3 = [[UIView alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     
-    for (int i=0; i<=[picArray count]-1; i++) {
+    NSArray *vArray = [[NSArray alloc] initWithObjects:v1, v2, v3, nil];
+    
+    for (int i=0; i<=[vArray count]-1; i++) {
         
-        UIImage *image = [picArray objectAtIndex:i];
+        UIView *v = [vArray objectAtIndex:i];
         CGRect frm = [[UIScreen mainScreen] bounds];
         frm.origin.x = (i*scrollView.frame.size.width);
+        v.frame = frm;
         
-        UIImageView *imgV = [[UIImageView alloc] initWithFrame:frm];
-        imgV.contentMode=UIViewContentModeScaleToFill;
-        [imgV setImage:image];
-        
-        [scrollView addSubview:imgV];
+        UIButton *button = [[UIButton alloc] init];
+        button.frame = CGRectMake(0, 0, 100, 100);
+        [button setTitle:@"Power On" forState:UIControlStateNormal];
+        button.layer.borderColor=[UIColor redColor].CGColor;
+        button.layer.borderWidth=2.0f;
+        button.layer.cornerRadius = button.bounds.size.width/2;
+
+        [v addSubview:button];
+        [scrollView addSubview:v];
     }
     
     CGSize frame = scrollView.frame.size;
-    frame.width = frame.width * [picArray count] - 1;
+    frame.width = frame.width * [vArray count] - 1;
     [scrollView setContentSize:frame];
 }
 
